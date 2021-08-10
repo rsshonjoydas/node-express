366408a8bf630ea39dac07f91c99f06379c23af6
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-10 14:49:51 +0600

    user login

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
 const userSchema = require('../schemas/userSchema');
@@ -21,6 +22,48 @@ router.post('/signup', async (req, res) => {
   } catch {
     res.status(500).json({
       message: 'Signup failed!',
+    });
+  }
+});
+
+// ! login
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.find({ username: req.body.username });
+    if (user && user.length > 0) {
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user[0].password
+      );
+      if (isValidPassword) {
+        const token = jwt.sign(
+          {
+            username: user[0].username,
+            userId: user[0]._id,
+          },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: '1h',
+          }
+        );
+
+        res.status(200).json({
+          access_token: token,
+          message: 'Login successful!',
+        });
+      } else {
+        res.status(401).json({
+          error: 'Authentication failed!',
+        });
+      }
+    } else {
+      res.status(401).json({
+        error: 'Authentication failed!',
+      });
+    }
+  } catch (error) {
+    res.status(401).json({
+      error: 'Authentication failed!',
     });
   }
 });
