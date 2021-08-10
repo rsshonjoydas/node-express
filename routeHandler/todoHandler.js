81955a0a8a2b9ac2a7d2b42b01f5fd7da6600483
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-10 08:04:16 +0600

    get data

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -5,7 +5,20 @@ const todoSchema = require('../schemas/todoSchema');
 const Todo = new mongoose.model('Todo', todoSchema);
 
 // ! get all the todo
-router.get('/', async (req, res) => {});
+router.get('/', async (req, res) => {
+  await Todo.find({ status: 'active' }, (err, data) => {
+    if (err) {
+      res.status(500).json({
+        error: 'There was a server side error!',
+      });
+    } else {
+      res.status(200).json({
+        result: data,
+        message: 'Todo get successfully!',
+      });
+    }
+  });
+});
 
 // ! get a todo by id
 router.get('/:id', async (req, res) => {});
@@ -50,7 +63,7 @@ router.put('/:id', async (req, res) => {
         status: 'active',
       },
     },
-    {useFindAndModify: false},
+    { useFindAndModify: false },
     (err) => {
       if (err) {
         res.status(500).json({
